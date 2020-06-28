4e52f5b6bfc18b8ae197a039a49ec938201f5e0e
unknown <ranyunlong>
2020-06-28 09:14:02 +0800

    update

---
+++ src/renderer/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {HashRouter} from "react-router-dom";
+
+
+ReactDOM.render(
+        <div className="app">
+            <h1>App</h1>
+            <HashRouter>
+
+            </HashRouter>
+        </div>,
+    document.getElementById('root')
+);
