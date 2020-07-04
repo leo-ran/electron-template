8dca56c8b88af265a07a4d424e49661b1b40db28
ranyunlong <service@ranyunlong.com>
2020-07-04 11:22:54 +0800

    update

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,8 +2,8 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 
 const dirname = path.dirname(__dirname);
-const file = path.join(dirname, "renderer", "index.html")
-const winURL = process.env.NODE_ENV === "development"  ? "http://localhost:9080" : `file://${file}`
+const file = path.join(dirname, "renderer", "index.html");
+const winURL = process.env.NODE_ENV === "development"  ? "http://localhost:9080" : `file://${file}`;
 
 let mainWindow: BrowserWindow | null;
 
@@ -20,37 +20,37 @@ function createWindow(){
             // 此设置运行渲染进程 使用node Api
             nodeIntegration: true,
         }
-    })
+    });
 
-    mainWindow.loadURL(winURL)
+    mainWindow.loadURL(winURL);
 
 
     mainWindow.on("closed", () => {
-        mainWindow = null
-        app.exit()
-    })
+        mainWindow = null;
+        app.exit();
+    });
 
     mainWindow.on("ready-to-show", () => {
         if (mainWindow) {
-            mainWindow.show()
+            mainWindow.show();
         }
     })
 
-    mainWindow.flashFrame(true)
+    mainWindow.flashFrame(true);
 }
 
 
 
-app.on("ready", createWindow)
+app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit()
     }
-})
+});
 
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
